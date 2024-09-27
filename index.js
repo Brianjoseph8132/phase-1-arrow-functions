a4fe51ce14051d45f1a7b0d573d97b29dcbfa1e2
Brian Joseph <brianjoseph8132@gmail.com>
2024-09-27 08:51:00 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+let a = 2000;
+let b = 100;
+const divide =(a,b) => (a/b)
+console.log(divide(2000,100))
+
+let x = 2
+const square = (x) => (x**x)
+console.log(square(2))
+
+{let a = 3
+ let b = 4
+ const add =(a,b) => (a + b);
+console.log(add(3,4))
+}
